7a2ae08bf90a30e3ffe9b1151b0cef8d9ca13049
ribeiro <polribeiro@live.fr>
2020-05-29 18:40:05 +0200

    pons result model

---
+++ app/src/main/java/com/paulribe/memowords/model/pons/Translation.java
@@ -0,0 +1,32 @@
+package com.paulribe.memowords.model.pons;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Translation {
+
+    @SerializedName("source")
+    private String source;
+    @SerializedName("target")
+    private String target;
+
+    public Translation(String source, String target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
